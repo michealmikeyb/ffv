06d52ffe2e19db94cdbf1137a29fc80ebc965658
Michael Mitchell <michael@mbmitchell.me>
2022-11-22 00:23:39 -0800

    change storage and version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -f Dockerfile -t ffv-backend:latest .
+docker build -f Dockerfile -t ffv-backend:0.1.0 .
 docker save ffv-backend:latest > ~/ffv-backend.tar
 microk8s ctr image import ~/ffv-backend.tar
