b5765de53c58fd3e398d01142dba5f07cd22a182
Michael Mitchell <michael@mbmitchell.me>
2022-11-21 22:52:44 -0800

    add home path

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -f Dockerfile -t ffv-backend:latest .
-docker save ffv-backend:latest > ffv-backend.tar
-microk8s ctr image import ffv-backend.tar
+docker save ffv-backend:latest > ~/ffv-backend.tar
+microk8s ctr image import ~/ffv-backend.tar
