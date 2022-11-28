6d42e999dcfdcb7381fdafdb7ec7dc2db05ac961
Michael Mitchell <michael@mbmitchell.me>
2022-11-28 10:18:21 -0800

    add logging

--- utils/utils.go
+++ utils/utils.go
@@ -1,12 +1,14 @@
 package utils
 
 import (
+	"log"
 	"os"
 
 	"github.com/gocql/gocql"
 )
 
 func GetCassandraSession() (*gocql.Session, error) {
+	log.Printf("Connecting to %s", os.Getenv("CASS_HOST"))
 	cluster := gocql.NewCluster(os.Getenv("CASS_HOST"))
 	return cluster.CreateSession()
 
